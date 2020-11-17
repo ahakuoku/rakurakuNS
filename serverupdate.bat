51571c0898226a5553a05aed2468749f6d2dd5af
ahakuoku <ahakuoku@gmail.com>
2020-11-17 18:06:46 +0900

    修正：試行錯誤

--- serverupdate.bat
+++ serverupdate.bat
@@ -10,10 +10,19 @@ if !usingnettool! == 0 (
 ) 
 
 set /p new_filename="新しい本体のファイル名を入力してください。「n」と入力するとキャンセルします。"
-if ""!new_filename!" == "n" (
+if "!new_filename!" == "n" (
 	echo "!strt_check!：処理を中止しました。"
 	pause >nul
-	exit /b 0	
+	exit /b 0
+) 
+
+rem set !exename:~1,-1!
+
+set /p exename="起動中の本体のファイル名を入力してください。「n」と入力するとキャンセルします。"
+if "!new_filename!" == "n" (
+	echo "!strt_check!：処理を中止しました。"
+	pause >nul
+	exit /b 0
 ) 
 
 set /p strt_check="サーバーの本体更新を開始します。よろしいですか？（Y/N）： "
@@ -25,7 +34,9 @@ if "!strt_check!" == "y" (
 	nettool -p !nettoolpass! -s !serverip! say "Maintenance start."
 	nettool -p !nettoolpass! -s !serverip! shutdown
 	ren !exename! server_old.exe
+	timeout /t 1 /nobreak >nul
 	ren !new_filename! !exename!
+	timeout /t 2 /nobreak >nul
 	start autostart.bat
 ) else (
 	echo "!strt_check!：処理を中止しました。"
