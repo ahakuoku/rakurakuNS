4cb7bd3e864196cd225bda8c11e76113b4e21207
ahakuoku <ahakuoku@gmail.com>
2021-03-27 02:14:16 +0900

    コメント付加

--- autoclose.bat
+++ autoclose.bat
@@ -2,22 +2,26 @@
 setlocal enabledelayedexpansion
 
 call setting.bat
-
+rem ここまでおまじない
+rem 設定次第で弾く
 if !usingnettool! == 0 (
 	echo このbatはsetting.bat内のusingnettoolを1にしないと使用できません。Enterキーを押すと終了します。
 	pause >nul
 	exit /b 0
 ) 
 
+rem 自動終了部分
 set /p strt_check="メンテナンスを開始します。よろしいですか？（M/C/N）： "
 if "!strt_check!" == "m" (
+	rem メンテナンスの場合
 	nettool -p !nettoolpass! -s !serverip! say "Maintenance soon."
 	echo [!DATE! !TIME!]メッセージを送信しました。自動再起動ツールは落としましたか？
 	timeout /t 30 /nobreak >nul
 	call module\autosave.bat
 	nettool -p !nettoolpass! -s !serverip! say "Maintenance start."
 	nettool -p !nettoolpass! -s !serverip! shutdown
-) else if "!strt_check!"=="c" ( 
+	) else if "!strt_check!"=="c" ( 
+	rem サーバー終了の場合
 	nettool -p !nettoolpass! -s !serverip! say "Server close soon."
 	echo [!DATE! !TIME!]メッセージを送信しました。自動再起動ツールは落としましたか？
 	timeout /t 30 /nobreak >nul
@@ -25,6 +29,7 @@ if "!strt_check!" == "m" (
 	nettool -p !nettoolpass! -s !serverip! say "Server closed. Thank you for playing at Server."
 	nettool -p !nettoolpass! -s !serverip! shutdown
 ) else (
+	rem それ以外の場合
 	echo "!strt_check!：処理を中止しました。"
 	pause >nul
 	exit /b 0
--- autosave.bat
+++ autosave.bat
@@ -2,7 +2,8 @@
 setlocal enabledelayedexpansion
 
 call setting.bat
-
+rem ここまでおまじない
+rem 設定次第で弾く
 if !usingnettool! == 0 (
 	echo このbatはsetting.bat内のusingnettoolを1にしないと使用できません。Enterキーを押すと終了します。
 	pause >nul
@@ -14,15 +15,20 @@ if !autosaveinterval! lss 60 (
 	exit /b 0
 ) 
 
+rem 無限ループ部分
 :loop
+	rem 設定再読み込み→設定秒数から30秒引く
 	call setting.bat
 	set /a interval=!autosaveinterval!-30
+	rem メッセージ送信→30秒待機
 	nettool -p !nettoolpass! -s !serverip! say "Autosave soon."
 	echo [!DATE! !TIME!]メッセージを送信しました。
 	timeout /t 30 /nobreak >nul
+	rem オートセーブ
 	call module\worktime.bat START
 	call module\autosave.bat
 	call module\worktime.bat STOP
+	rem 設定秒数からオートセーブにかかった秒数を減算
 	set /a interval=!interval!-%DPS%
 	timeout /t "!interval!" /nobreak >nul
 goto loop
