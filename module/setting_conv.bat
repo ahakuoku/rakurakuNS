e12caadce570c53dea78f32706679a71afc024d2
ahakuoku <ahakuoku@gmail.com>
2021-03-28 14:55:17 +0900

    起動しないバグを修正

--- module/setting_conv.bat
+++ module/setting_conv.bat
@@ -2,105 +2,105 @@ call setting.bat
 if "!use_nettool!" == "" (
 	if "!usingnettool!" == "" (
 		set use_nettool=0
-	) else
+	) else (
 		set use_nettool=usingnettool
 	) 
 ) 
 if "!nettool_password!" == ""
 	if "!nettoolpass!" == "" (
 		set nettool_password=""
-	) else
+	) else (
 		set nettool_password=nettoolpass
 	) 
 ) 
 if "!launch_file!" == ""
 	if "!startfile!" == "" (
 		set launch_file=sim-WinGDI64-OTRP.exe
-	) else
+	) else (
 		set launch_file=startfile
 	) 
 ) 
 if "!server_save!" == ""
 	if "!serverfile!" == "" (
 		set server_save=server13353-network.sve
-	) else
+	) else (
 		set server_save=serverfile
 	) 
 ) 
 if "!load_save!" == ""
 	if "!autosave!" == "" (
 		set load_save=autosave-pak.sve
-	) else
+	) else (
 		set load_save=autosave
 	) 
 ) 
 if "!chack_exe!" == ""
 	if "!exename!" == "" (
 		set check_exe="sim-WinGDI64-OTRP.exe"
-	) else
+	) else (
 		set check_exe=exename
 	) 
 ) 
 if "!server_address!" == ""
 	if "!serverip!" == "" (
 		set server_address="127.0.0.1:13353"
-	) else
+	) else (
 		set server_address=serverip
 	) 
 ) 
 if "!topmost_company_password!" == ""
 	if "!0companypass!" == "" (
 		set topmost_company_password=""
-	) else
+	) else (
 		set topmost_company_password=0companypass
 	) 
 ) 
 if "!autosave_interval!" == ""
 	if "!autosaveinterval!" == "" (
 		set autosave_interval=""
-	) else
+	) else (
 		set autosave_interval=autosaveinterval
 	) 
 ) 
 if "!conflict_error_avoidance!" == ""
 	if "!pressspacekey!" == "" (
 		set conflict_error_avoidance=""
-	) else
+	) else (
 		set conflict_error_avoidance=pressspacekey
 	) 
 ) 
 if "!ban_address_1!" == ""
 	if "!ban-ip1!" == "" (
 		set ban_address_1=""
-	) else
+	) else (
 		set ban_address_1=ban-ip1
 	) 
 ) 
 if "!ban_address_2!" == ""
 	if "!ban-ip2!" == "" (
 		set ban_address_2=""
-	) else
+	) else (
 		set ban_address_2=ban-ip2
 	) 
 ) 
 if "!ban_address_3!" == ""
 	if "!ban-ip3!" == "" (
 		set ban_address_3=""
-	) else
+	) else (
 		set ban_address_3=ban-ip3
 	) 
 ) 
 if "!ban_address_4!" == ""
 	if "!ban-ip4!" == "" (
 		set ban_address_4=""
-	) else
+	) else (
 		set ban_address_4=ban-ip4
 	) 
 ) 
 if "!ban_address_5!" == ""
 	if "!ban-ip5!" == "" (
 		set ban_address_5=""
-	) else
+	) else (
 		set ban_address_5=ban-ip5
 	) 
 ) 
