3e145ce12e5cf21bff7a4ee7a630332fb649c61e
ahakuoku <ahakuoku@gmail.com>
2024-03-29 22:29:32 +0900

    修正：誤った挙動をする問題

--- module/backup.bat
+++ module/backup.bat
@@ -9,8 +9,8 @@ if exist autosave\autosave_100.sve (
 set autosave_backup_before=99
 set autosave_backup_after=100
 for /l %%i in (1,1,99) do (
-    if exist autosave\autosave_%%i.sve (
-        ren autosave\autosave_%%i.sve autosave_%autosave_backup_after%.sve
+    if exist autosave\autosave_%autosave_backup_before%.sve (
+        ren autosave\autosave_%autosave_backup_before%.sve autosave_%autosave_backup_after%.sve
     ) 
     set /a autosave_backup_before-=1
     set /a autosave_backup_after-=1
