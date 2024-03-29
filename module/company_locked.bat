ad2cb1b568b716bb4ca559ee4cc717561f14cee7
ahakuoku <ahakuoku@gmail.com>
2024-03-29 23:11:59 +0900

    Revert "修正：会社の存在チェックが正しく動作しない問題"
    This reverts commit 52523a94e93a2a35f218afbe4867847fc38e072a.

--- module/company_locked.bat
+++ module/company_locked.bat
@@ -3,19 +3,19 @@ rem nettoolを使用する設定において、会社IDが-1以外に設定されていた場合にのみ適用
 if !use_nettool! == 1 (
 	if not !company_password_1_number! == -1 (
 		for /f "usebackq delims=" %%A in (`nettool -p !nettool_password! -s !server_address! info-company !company_password_1_number!`) do set company_existence=%%A
-		if not !company_existence! == "Nothing received. " (
+		if not !company_existence! == "Nothing received." (
 			nettool -p !nettool_password! -s !server_address! lock-company !company_password_1_number! !company_password_1!
 		) 
 	) 
 	if not !company_password_2_number! == -1 (
 		for /f "usebackq delims=" %%A in (`nettool -p !nettool_password! -s !server_address! info-company !company_password_2_number!`) do set company_existence=%%A
-		if not !company_existence! == "Nothing received. " (
+		if not !company_existence! == "Nothing received." (
 			nettool -p !nettool_password! -s !server_address! lock-company !company_password_2_number! !company_password_2!
 		) 
 	) 
 	if not !company_password_3_number! == -1 (
 		for /f "usebackq delims=" %%A in (`nettool -p !nettool_password! -s !server_address! info-company !company_password_3_number!`) do set company_existence=%%A
-		if not !company_existence! == "Nothing received. " (
+		if not !company_existence! == "Nothing received." (
 			nettool -p !nettool_password! -s !server_address! lock-company !company_password_3_number! !company_password_3!
 		) 
 	) 
